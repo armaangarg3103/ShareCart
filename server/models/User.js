b7bd4126964cb6dc8f9bdee1154d28ec1084bad7
unknown <Dell@DESKTOP-36NHGA7.(none)>
2025-12-02 15:43:12 +0530

    Optimize login speed (reduce bcrypt rounds 10->8) and fix navbar logout redirect to home page

--- server/models/User.js
+++ server/models/User.js
@@ -125,7 +125,7 @@ userSchema.pre('save', async function(next) {
     next();
   }
   
-  const salt = await bcrypt.genSalt(10);
+  const salt = await bcrypt.genSalt(8);
   this.password = await bcrypt.hash(this.password, salt);
 });
 
